6c784a7f8b67ce8be5a3cbfb3074c2add2079b04
Denis <jtz_dennis@yahoo.com>
2024-12-16 23:06:55 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -15,7 +15,10 @@ navToggle.addEventListener("click", () => {
 
 function changePlanetDetails(planet) {
   const img = document.getElementById("planetImage");
-  const p = document.getElementById("planetDescription");
+  const description = document.getElementById("planetDescription");
+  const name = document.getElementById("planetName");
+  const distance = document.getElementById("averageDistance");
+  const travel = document.getElementById("travelTime");
 
   img.src = DATA.destinations.filter(
     (item) => item.name === planet
@@ -23,5 +26,59 @@ function changePlanetDetails(planet) {
 
   description.innerHTML = DATA.destinations.filter(
     (item) => item.name === planet
+  )[0].description;
+
+  name.innerHTML = DATA.destinations.filter(
+    (item) => item.name === planet
+  )[0].name;
+
+  distance.innerHTML = DATA.destinations.filter(
+    (item) => item.name === planet
+  )[0].distance;
+
+  travel.innerHTML = DATA.destinations.filter(
+    (item) => item.name === planet
+  )[0].travel;
+}
+
+
+function changeCrewDetails(crew) {
+  const img = document.getElementById("crewImage");
+  const bio = document.getElementById("crewBio");
+  const role = document.getElementById("crewRole");
+  const name = document.getElementById("crewName");
+  
+  img.src = DATA.crew.filter(
+    (item) => item.name === crew
+  )[0].images.png;
+
+  bio.innerHTML = DATA.crew.filter(
+    (item) => item.name === crew
+  )[0].bio;
+
+  role.innerHTML = DATA.crew.filter(
+    (item) => item.name === crew
+  )[0].role;
+
+  name.innerHTML = DATA.crew.filter(
+    (item) => item.name === crew
+  )[0].name;
+}
+
+function changeTechnologyDetails(technology) {
+  const img = document.getElementById("technologyImage");
+  const name = document.getElementById("technologyName");
+  const description = document.getElementById("technologyDescription");
+
+  img.src = DATA.technology.filter(
+    (item) => item.name === technology
+  )[0].images.landscape
+
+  name.innerHTML = DATA.technology.filter(
+    (item) => item.name === technology
+  )[0].name;
+
+  description.innerHTML = DATA.technology.filter(
+    (item) => item.name === technology
   )[0].description;
 }
